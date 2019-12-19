7220710dd781fae3fcea71bb093017b7ea47f63f
Edilon Andrade <edilon.andrade_ext@stone.com.br>
2019-12-19 18:29:26 -0300

    Criando a primeira teoria.

--- MyFirstUnitTests/MyFirstUnitTests/MyFirstUnitTests/UnitTest1.cs
+++ MyFirstUnitTests/MyFirstUnitTests/MyFirstUnitTests/UnitTest1.cs
@@ -21,5 +21,19 @@ namespace MyFirstUnitTests
         {
             return x + y;
         }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(IsOdd(value));
+        }
+
+        bool IsOdd(int value)
+        {
+            return value % 2 == 1;
+        }
     }
 }
