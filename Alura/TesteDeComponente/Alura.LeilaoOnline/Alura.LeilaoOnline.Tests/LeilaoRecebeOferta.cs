5e69f05e694f13fcc8917092ccdaedccfb122fcb
Edilon Andrade <edilonandrade@gmail.com>
2020-03-21 11:14:09 -0300

    Refatorando o teste de recebe leilão para utilizar teoria.

--- Alura/TesteDeComponente/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
+++ Alura/TesteDeComponente/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -6,21 +6,28 @@ namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoRecebeOferta
     {
-        [Fact]
-        public void NaoPermiteNovosLancesDadoLeilaoFinalizado()
+        [Theory]
+        [InlineData(4, new double[] { 100, 1200, 1400, 1300 })]
+        [InlineData(2, new double[] { 800, 900 })]
+
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado(
+            int qtdeEsperada,
+            double[] ofertas)
         {
             //Arranje - Cenário
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(fulano, 900);
+
+            foreach (var valor in ofertas)
+                leilao.RecebeLance(fulano, valor);
+
             leilao.TerminaPregao();
 
             //Act - método sob teste
             leilao.RecebeLance(fulano, 1000);
 
             //Assert
-            Assert.Equal(2, leilao.Lances.Count());
+            Assert.Equal(qtdeEsperada, leilao.Lances.Count());
         }
     }
 }
