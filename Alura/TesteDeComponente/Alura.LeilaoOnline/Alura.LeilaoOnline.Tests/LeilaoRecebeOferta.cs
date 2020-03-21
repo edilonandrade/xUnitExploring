1d462335135e1ef1447ef2bef6c7a57c6cdffa09
Edilon Andrade <edilonandrade@gmail.com>
2020-03-21 11:07:53 -0300

    Inclusão do teste no recebe lance, quando o leilão terminou.

---
+++ Alura/TesteDeComponente/Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Alura.LeilaoOnline.Core;
+using System.Linq;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoRecebeOferta
+    {
+        [Fact]
+        public void NaoPermiteNovosLancesDadoLeilaoFinalizado()
+        {
+            //Arranje - Cenário
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(fulano, 900);
+            leilao.TerminaPregao();
+
+            //Act - método sob teste
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert
+            Assert.Equal(2, leilao.Lances.Count());
+        }
+    }
+}
